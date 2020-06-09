7c15ee0efeb881f56c006a810d26059733596dcd
Timur Garipov <tbgaripov@gmail.com>
2020-06-09 19:10:45 +0300

    Изменяет l на L в Layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 
